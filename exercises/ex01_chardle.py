e497bdc61c3bd10b3bc921fa6fc8ae46e5eefeb8
Kristin Wisniewski <kcwstarw@ad.unc.edu>
2022-01-20 18:33:33 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,33 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730319038"
+
+help_me: str = input("Enter a 5-character word:")
+len(help_me) == 5
+if len(help_me) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+save_me: str = input("Enter a single character:")
+len(save_me) == 1
+if len(save_me) != 1:
+    print("Error: Character must be a single character")
+    exit()
+lets_go: int = 0
+print("Searching for " + save_me + " in " + help_me)
+
+if save_me == help_me[0]:
+    print(save_me + " found at index " + str(0))
+    lets_go = lets_go + 1
+if save_me == help_me[1]:
+    print(save_me + " found at index " + str(1))
+    lets_go = lets_go + 1
+if save_me == help_me[2]:
+    print(save_me + " found at index " + str(2))
+    lets_go = lets_go + 1
+if save_me == help_me[3]:
+    print(save_me + " found at index " + str(3))
+    lets_go = lets_go + 1
+if save_me == help_me[4]:
+    print(save_me + " found at index " + str(4))
+    lets_go = lets_go + 1
+
+print(str(lets_go) + " instances of " + str(save_me) + " found at " + str(help_me))
